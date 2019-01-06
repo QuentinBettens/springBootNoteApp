5eecf89c9f6cb70b876e49733c1b4652f52d690c
Quentin Bts <bettens.q@gmail.com>
2019-01-06 18:59:56 +0100

    Initial commit

---
+++ src/main/java/be/bettens/springBootNoteApp/SpringBootNoteAppApplication.java
@@ -0,0 +1,14 @@
+package be.bettens.springBootNoteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNoteAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/be/bettens/springBootNoteApp/SpringBootNoteAppApplicationTests.java
@@ -0,0 +1,17 @@
+package be.bettens.springBootNoteApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootNoteAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
